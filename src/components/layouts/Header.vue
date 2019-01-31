48df169d5fe2d4e3a416903bb999e49864d7e1c2
princeallan <allankirui57@gmail.com>
2019-01-31 13:01:43 +0300

    Initial vue project commit

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <header>
+            <div id="nav">
+                <router-link to="/">Home</router-link> |
+                <router-link to="/about">About</router-link>
+            </div>
+        </header>
+    </div>
+</template>
