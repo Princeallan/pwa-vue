83653ba652df918213a14a5b8319a918f2911255
Princeallan <allankirui57@gmail.com>
2019-02-16 01:20:16 +0300

    Update configFirebase.js

--- src/configFirebase.js
+++ src/configFirebase.js
@@ -4,12 +4,7 @@ import 'firebase/messaging';
 import 'firebase/storage';
 
 const config = {
-    apiKey: "AIzaSyAEfdvV_sMrDeY42Gu-XFRfdF3u7omLuAU",
-    authDomain: "vue-pwa-d4925.firebaseapp.com",
-    databaseURL: "https://vue-pwa-d4925.firebaseio.com",
-    projectId: "vue-pwa-d4925",
-    storageBucket: "vue-pwa-d4925.appspot.com",
-    messagingSenderId: "1026370344024"
+    
   };
   firebase.initializeApp(config);
 
